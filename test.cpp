0a9ba2e14dfbe71a0719bad4dadfb517c1b65d35
caitlin-allison <bantansonyeondan12@gmail.com>
2025-02-24 15:13:14 -0600

    Added function

--- test.cpp
+++ test.cpp
@@ -1,6 +1,13 @@
 #include <iostream>;
 
+void printHelloWorld()
+{
+    std::cout << "Hello World" << std::endl;
+}
+
 int main()
 {
     std::cout << "I am new file" << std::endl;
+
+    printHelloWorld();
 }
