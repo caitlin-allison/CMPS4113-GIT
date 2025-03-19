51100ed861239529d00ff4703ed778dcf516e66b
caitlin-allison <bantansonyeondan12@gmail.com>
2025-03-19 11:31:05 -0500

    More in-depth changes

--- source.cpp
+++ source.cpp
@@ -5,13 +5,40 @@
 #include <string>
 
 using namespace std;
+void printShape(Shape *shape);
+void printHeading();
+int add(int a, int b);
 
 int main()
 {
     Shape *shape = new Square(10.0);
     cout << "Area: " << shape->getArea() << endl;
-    cout << "Hello from Branch newClasses" << endl;
-    cout << "I am the experimental branch" << endl;
+    printHeading();
+
+    printShape(shape);
+
+    shape = new Circle(10.0);
+    printShape(shape);
+
+    cout << "Perimeter: " << shape->getPerimeter() << endl;
+    cout << "Sum: " << add(1, 2) << endl;
+
+    delete shape;
 
     return 0;
+}
+
+void printShape(Shape *shape)
+{
+    cout << "Area: " << shape->getArea() << endl;
+}
+
+void printHeading()
+{
+    cout << "Hello from branch newClasses" << endl;
+}
+
+int add(int a, int b)
+{
+    return a + b;
 }
